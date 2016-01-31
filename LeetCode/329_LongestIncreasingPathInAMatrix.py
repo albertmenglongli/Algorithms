9f5ce723db8aaeaf2959cd8557b821b902760808
limenglong <albert.menglongli@gmail.com>
2016-01-31 12:27:32 +0800

    code format, pylint related

--- LeetCode/329_LongestIncreasingPathInAMatrix.py
+++ LeetCode/329_LongestIncreasingPathInAMatrix.py
@@ -9,9 +9,9 @@ class Solution(object):
                 val = matrix[i][j]
                 dp[i][j] = 1 + max(
                     dfs(i - 1, j) if i and val > matrix[i - 1][j] else 0,
-                    dfs(i + 1, j) if i< M-1 and val > matrix[i + 1][j] else 0,
+                    dfs(i + 1, j) if i < M-1 and val > matrix[i + 1][j] else 0,
                     dfs(i, j - 1) if j and val > matrix[i][j - 1] else 0,
-                    dfs(i, j +1) if j< N-1 and val > matrix[i][j + 1] else 0)
+                    dfs(i, j + 1) if j < N-1 and val > matrix[i][j + 1] else 0)
             return dp[i][j]
 
         if not matrix or not matrix[0]: return 0
@@ -21,8 +21,8 @@ class Solution(object):
         
 if __name__ == "__main__":
     nums = [
-      [9,9,4],
-      [6,6,8],
-      [2,1,1]
+        [9, 9, 4],
+        [6, 6, 8],
+        [2, 1, 1]
     ]
     print Solution().longestIncreasingPath(nums)

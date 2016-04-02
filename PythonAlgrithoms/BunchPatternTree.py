39fc8711beb9b09ace950abf70eac3ec939cdf85
limenglong <albert.menglongli@gmail.com>
2016-04-02 20:39:50 +0800

    remove comments

--- PythonAlgrithoms/BunchPatternTree.py
+++ PythonAlgrithoms/BunchPatternTree.py
@@ -1,11 +1,3 @@
-"""
-When prototyping (or even finalizing) data structures such as trees, it can be useful to have a flexible
-class that will allow you to specify arbitrary attributes in the constructor. 
-In these cases, the “Bunch” pattern (named by Alex Martelli in the Python Cookbook) can come in handy.
-There are many ways of implementing it, but the gist of it is the following:
-"""
-
-
 class Bunch(dict):
     """docstring for Bunch"""
 
@@ -17,10 +9,3 @@ T = Bunch
 t = T(left=T(left="a", right="b"), right=T(left="c"))
 print t.left.right
 print t['left']['right']
-
-
-"""
-This pattern isn’t useful only when building trees, of course.
-You could use it for any situation where you’d want a flexible object
-whose attributes you could set in the constructor.
-"""

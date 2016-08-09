8a457832ff0851fdd7449db0dd4374e652d649d1
limenglong <albert.menglongli@gmail.com>
2016-08-09 23:56:31 +0800

    Sum of two integers, without +/-

---
+++ LeetCode/lc_371_SumOfTwoIntegers.py
@@ -0,0 +1,32 @@
+class Solution(object):
+
+    def getSum(self, a, b):
+        """
+        recursion version, just for understanding
+        :type a: int
+        :type b: int
+        :rtype: int
+        >>> Solution().getSum(2, 1)
+        3
+        >>> Solution().getSum(3, 1)
+        4
+        >>> Solution().getSum(-1, 1)
+        0
+        """
+        # 32 bits integer max
+        MAX = 0x7FFFFFFF
+        # 32 bits interger min
+        MIN = 0x80000000
+        # mask to get last 32 bits
+        mask = 0xFFFFFFFF
+        while b != 0:
+            # ^ get different bits and & gets double 1s, << moves carry
+            a, b = (a ^ b) & mask, ((a & b) << 1) & mask
+        # if a is negative, get a's 32 bits complement positive first
+        # then get 32-bit positive's Python complement negative
+        return a if a <= MAX else ~(a ^ mask)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
--- LeetCode/lc_71_SimplifyPath.py
+++ LeetCode/lc_71_SimplifyPath.py
@@ -1,4 +1,5 @@
 class Solution(object):
+
     @staticmethod
     def simplifyPath(path):
         """

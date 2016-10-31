5334585d04f726520493e0bf02102b05b7f24b1e
Menglong.Li <albert.menglongli@gmail.com>
2016-10-31 23:44:17 +0800

    Update code_chef_INO1402.py

--- Others/code_chef_INO1402.py
+++ Others/code_chef_INO1402.py
@@ -7,7 +7,11 @@ def build_graph(edge_info, num_V):
 def floyd(G, num_V):
     for k in range(num_V):
         for i in range(num_V):
+            if i == k:
+                continue
             for j in range(num_V):
+                if j == i or j == k:
+                    continue
                 new_weight = G[i][k] + G[k][j]
                 if G[i][j] > new_weight:
                     G[i][j] = new_weight
@@ -17,11 +21,8 @@ num_V = int(line_1_input.split(" ")[0])
 num_E = int(line_1_input.split(" ")[1])
 edge_info = list()
 for _ in range(num_E):
-    line_x_input = raw_input("")
-    line_x_input = line_x_input.strip()
+    line_x_input = raw_input("").strip()
     edge_info.append((int(line_x_input.split(" ")[0]) - 1, int(line_x_input.split(" ")[1]) - 1, int(line_x_input.split(" ")[2])))
 
-G = build_graph(edge_info, num_V)
-floyd(G, num_V)
-
-print G[0][num_V - 1]
+floyd(build_graph(edge_info, num_V), num_V)
+print max([max(filter(lambda x: x != float('inf'), sub_array)) for sub_array in G])

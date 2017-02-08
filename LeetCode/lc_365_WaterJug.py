47c983df2fe50f81e69115e7667ba831c897f7f0
menglong.li <menglong.li@funplus.com>
2017-02-09 00:17:45 +0800

    water jug

---
+++ LeetCode/lc_365_WaterJug.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canMeasureWater(self, x, y, z):
+        if z == 0:
+            return True
+        if x == 0 or y == 0:
+            return False
+        max = x + y
+        if x < y:
+            x, y = y, x
+        while y:
+            temp = x % y
+            x = y
+            y = temp
+
+        return True if z % x == 0 and z <= max else False
+
+
+print Solution().canMeasureWater(6, 9, 300)

64af0ee06c002c389cdd88ba75e838dbdc63325c
menglong.li <menglong.li@funplus.com>
2017-02-09 20:28:39 +0800

    lc_105_ConstructBinaryTreeFromPreorderAndInorderTraversal lc_484_Find_Permutation

---
+++ LeetCode/lc_105_ConstructBinaryTreeFromPreorderAndInorderTraversal.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        if not preorder:
+            return None
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1: mid + 1], inorder[0: mid])
+        root.right = self.buildTree(preorder[mid + 1:], inorder[mid + 1:])
+
+        return root
---
+++ LeetCode/lc_484_Find_Permutation.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+class Solution(object):
+    def findPermutation(self, s):
+        """
+        :type s: str
+        :rtype: List[int]
+        """
+        output = [1]
+        c = 0
+        idx_of_last_adjacent_D = 0
+        while c < len(s):
+            if s[c] == 'I':
+                # 如果下一个数是递增的, 直接将新的最大值追加到末尾, 新的最大值为 c + 2
+                output.append(c + 2)
+                # 告诉下一次循环, 上一次的数字不是递减的, 连续可追溯到的D的索引
+                idx_of_last_adjacent_D = 'N/A'
+            elif s[c] == 'D':
+                # 如果下一个数是减小的, 分两种情况
+                if idx_of_last_adjacent_D == 'N/A':
+                    # 如果之前不是递减的, 标记当前开始递减
+                    idx_of_last_adjacent_D = c
+                    output.append(c + 2)
+                    # 交换最后两个值(上一次的值, 和新插入的值),
+                    # 由于上一次是递增的, 且值 c + 1, c + 2 在数组中都是最大的,
+                    # 所以他们的交换不会破坏之前数字之前的关系, 还满足了递减关系, 且组成的数字数组排序最小
+                    output[c], output[c + 1] = output[c + 1], output[c]
+                else:
+                    # 如果之前一直都是递减的, 那么则需要将新的最大值,插入到之前标记的, 递减开始的地方,
+                    # 可以保证满足了递减关系的同时, 且组成的数字数组排序最小
+                    output.insert(idx_of_last_adjacent_D, c + 2)
+            c += 1
+        return output
+
+
+print Solution().findPermutation("DDIIDI")

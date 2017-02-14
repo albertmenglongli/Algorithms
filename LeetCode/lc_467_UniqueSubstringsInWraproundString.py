eb1f4c60ac672ef81902b3b46d5b4683681a2168
menglong.li <menglong.li@funplus.com>
2017-02-14 20:51:26 +0800

    LeetCode/lc_467_UniqueSubstringsInWraproundString.py

---
+++ LeetCode/lc_467_UniqueSubstringsInWraproundString.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findSubstringInWraproundString(self, p):
+        """
+        :type p: str
+        :rtype: int
+        """
+        table = [0] * 26
+        cur_max = 0
+        for i, value in enumerate(p):
+            if i > 0 and (ord(p[i - 1]) + 1) % 26 == ord(value) % 26:
+                cur_max += 1
+            else:
+                cur_max = 1
+            table[ord(value) - ord('a')] = max(table[ord(value) - ord('a')], cur_max)
+        return sum(table)
+
+print Solution().findSubstringInWraproundString("zab")

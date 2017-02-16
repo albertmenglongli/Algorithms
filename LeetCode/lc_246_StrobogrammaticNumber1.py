134204b48190563d62c45a52f7b28e14cd615ee2
menglong.li <menglong.li@funplus.com>
2017-02-16 18:16:25 +0800

    LeetCode/lc_246_StrobogrammaticNumber1.py

---
+++ LeetCode/lc_246_StrobogrammaticNumber1.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def isStrobogrammatic(self, num):
+        mapping = {
+            '0': '0',
+            '1': '1',
+            '6': '9',
+            '8': '8',
+            '9': '6'
+        }
+        """
+        :type num: str
+        :rtype: bool
+        """
+        n = len(num)
+        s = 0
+        e = n - 1
+        while s <= e:
+            if s == e:
+                if not num[s] in ('0', '1', '8'):
+                    return False
+                else:
+                    return True
+            if not num[s] in mapping:
+                return False
+            if mapping[num[s]] != num[e]:
+                return False
+
+            s += 1
+            e -= 1
+
+        return True
+
+print Solution().isStrobogrammatic('11')
